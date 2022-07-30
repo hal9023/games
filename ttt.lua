3a04d164e5cd55ccaddc6eabe5078653d99f1aa8
jprhea <jprhea37@gmail.com>
2022-07-29 19:08:05 -0500

    Removed 20 lines of un-needed code

--- ttt.lua
+++ ttt.lua
@@ -9,27 +9,6 @@ function drawExample()
 	drawBoard(m)
 end
 -- backend
-function charToInt(marker) 
-	if (marker == 'X') then
-		out = 1	
-	end
-	
-	if (marker == 'O') then
-		out = 2
-	end
-	return out
-		
-end
-function intToChar(number)
-	if (number == 1) then
-		out = 'X'
-	end	
-	if (number == 2) then
-		out = 'O'
-	end
-
-	return out
-end
 function check4win(markers, mark)
 	-- horizontal line cases ( - - -)
 	if (markers[1] == mark and markers[2] == mark and markers[3] == mark) then
